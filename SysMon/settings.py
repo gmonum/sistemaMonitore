b3ff98b529fafb6de5c579a2a63b646dce660e4e
rodrigo <rrramirez@gmail.com>
2018-12-29 16:30:11 -0600

    actualiza settings.py

--- SysMon/settings.py
+++ SysMon/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-	'static/templates',
+	'static/templates'
 	],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.static',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
@@ -119,6 +120,39 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'filters': {
+            'require_debug_false': {
+                '()': 'django.utils.log.RequireDebugFalse'
+            }
+        },
+        'handlers': {
+            'mail_admins': {
+                'level': 'ERROR',
+                'filters': ['require_debug_false'],
+                'class': 'django.utils.log.AdminEmailHandler'
+            },
+            'console':{
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler'
+            },
+        },
+        'loggers': {
+            'django.request': {
+                'handlers': ['mail_admins'],
+                'level': 'ERROR',
+                'propagate': True,
+                },
+            'SistemaMonitoreo': {
+                'handlers': ['console'],
+                'level': 'INFO'
+            },
+
+            }
+    }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
@@ -138,7 +172,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = 'https://storage.googleapis.com/sistema-monitoreo-rrr/static/'
-STATIC_ROOT = "static/"
+STATIC_ROOT = "/static/"
 
 DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 GS_BUCKET_NAME = 'multimediasistemamonitoreorrr'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    'static',
+)
+
